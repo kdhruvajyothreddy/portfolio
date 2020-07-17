4e956183f166b6cad3ecf09a0e8b6253a8f62ee8
kdhruvajyothreddy <kdhruvajyothreddy@gmail.com>
2020-07-17 21:23:03 +0530

    Adding model references for the Blog app and adding app on the project settings file

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+# Create Blog Models
+# title
+# pub_date
+# body
+# image
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+
+# Add the Blog app to the settings
+
+# Create a migration
+
+# Migrate
