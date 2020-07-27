ed7ac1fcb87ec4d167414bd1bbf40b922501cbba
kdhruvajyothreddy <kdhruvajyothreddy@gmail.com>
2020-07-27 17:11:19 +0530

    Change to show the BODY text upto only 100 characters

--- blog/models.py
+++ blog/models.py
@@ -13,11 +13,10 @@ class Blog(models.Model):
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
 
-
 # Add the Blog app to the settings
-
 # Create a migration
-
 # Migrate
-
 # Add to the admin
+
+    def summary(self):
+        return self.body[:100]
